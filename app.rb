fba3c2bce51983384356c5d0db7d057e31324540
danieloh <56809357+danieloh01@users.noreply.github.com>
2025-01-29 00:23:13 +0000

    final commit

--- app.rb
+++ app.rb
@@ -52,3 +52,15 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dice/:zebra/:giraffe") do
+  @num_dice = params.fetch("zebra").to_i
+  @sides = params.fetch("giraffe").to_i
+
+  @rolls = []
+  @num_dice.times do
+    die = rand(1..@sides)
+    @rolls.push(die)
+  end
+  erb(:flexible)
+end 
